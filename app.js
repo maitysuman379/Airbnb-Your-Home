e90269cb6b56ceede7281fca713b216706cafb23
maitysuman379 <maitysuman379@gmail.com>
2024-09-29 12:32:04 +0530

    error-handeling

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -29,6 +30,19 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+
+//Validation Joi 
+const validateListing = (req,res,next) =>{
+    let  {error} = listingSchema.validate(req.body);
+    if(error) {
+      let errMsg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(400,errMsg);
+    }else{
+      next();
+    }
+};
+
+//Root Route
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
@@ -52,23 +66,8 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-app.post("/listings", wrapAsync(async (req, res) => {
-  if(!req.body.listing){
-    throw new ExpressError(400,"Send valid data for listing");
-  };
+app.post("/listings", validateListing,wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
-  if(!newListing.title){
-    throw new ExpressError(400,"title is missing");
-  };
-  if(!newListing.description){
-    throw new ExpressError(400,"description is missing");
-  }
-  if(!newListing.location){
-    throw new ExpressError(400,"location is missing");
-  }
-  if(!newListing.country){
-    throw new ExpressError(400,"country is missing");
-  }
     await newListing.save();
     res.redirect("/listings");
 }));
@@ -81,10 +80,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-  if(!req.body.listing){
-    throw new ExpressError(400,"Send valid data for listing");
-  };
+app.put("/listings/:id", validateListing,wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
@@ -130,3 +126,18 @@ app.listen(8080, () => {
 //   res.send("successful testing");
 // });
 
+
+
+
+  // if(!newListing.title){
+  //   throw new ExpressError(400,"title is missing");
+  // };
+  // if(!newListing.description){
+  //   throw new ExpressError(400,"description is missing");
+  // }
+  // if(!newListing.location){
+  //   throw new ExpressError(400,"location is missing");
+  // }
+  // if(!newListing.country){
+  //   throw new ExpressError(400,"country is missing");
+  // }
