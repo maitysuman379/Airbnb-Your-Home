7d8f1db2bad78ada4a228c79ba60675975597e8a
maitysuman379 <maitysuman379@gmail.com>
2024-10-03 12:04:03 +0530

    models

--- init/index.js
+++ init/index.js
@@ -17,7 +17,7 @@ async function main() {
 };
 
 const initDB = async () => {
-  await Listing.deleteMany({});
+  // await Listing.deleteMany({});
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
---
+++ models/review.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    created_at: {
+        type: Date,
+        default:Date.now(),
+    }
+});
+
+module.exports = mongoose.model("Review",reviewSchema);
